267d4208a8a416b35a2b5b223d419837612f0bb5
Rui Ferreira <ferreira.rcb@gmail.com>
2017-02-16 22:48:28 +0200

    Add redirect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+import App from './components/App';
+import Home from './components/Home';
+import PageNotFound from './components/PageNotFound';
+import ExampleComponent from './components/ExampleComponent';
+import ExampleTwoDeepComponent from './components/ExampleTwoDeepComponent';
+
+
+const routes = (
+  <Route path="/" mapMenuTitle="Home" component={App}>
+    <IndexRoute component={Home} />
+
+    <Route path="example" mapMenuTitle="Example" component={ExampleComponent}>
+      <Route path="two-deep" mapMenuTitle="Two Deep" component={ExampleTwoDeepComponent} />
+    </Route>
+
+    <Route path="*" mapMenuTitle="Page Not Found" component={PageNotFound} />
+  </Route>
+);
+
+
+render(
+  <Router
+    history={browserHistory}
+    routes={routes}
+  />,
+  document.getElementById('root'),
 );
